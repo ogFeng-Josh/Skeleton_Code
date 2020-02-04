be206c8b1b2d5a3fb883db965d403848d6795e8b
Josh <36059029+PGz-Josh@users.noreply.github.com>
2020-02-04 15:27:19 -0800

    Add files via upload

---
+++ Skeleton.cpp
@@ -0,0 +1,55 @@
+//**********************************************************************
+//					Program is written by Joshua Yang
+//					Student ID is 2355145
+//					Professor of course is...
+//					Date is...
+//					Assignment objective is...
+//
+//
+//
+//
+//
+//
+//**********************************************************************
+
+
+//Libraries required/used for this
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include<ctime>
+#include<cctype>
+#include<cstdlib>
+
+//Function Declarations
+
+
+//Include Predefined Identifiers
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	//Data Declarations
+	
+	
+	
+	
+	
+	
+	//Return data
+	return 0;
+}
+
+//Algorithm Setup			|			Corresponding C++ Statements
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
