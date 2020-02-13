7f32f3ad20cc3ce02043eaed9799bb92b89ee24b
Josh <36059029+PGz-Josh@users.noreply.github.com>
2020-02-12 22:18:45 -0800

    Add files via upload

---
+++ multipleSources/body.cpp
@@ -0,0 +1,10 @@
+#include "head.h"
+
+//Use namespace in CPP instead of header
+using namespace std;
+
+
+void helloWorld()
+{
+	cout << "Hello World!" << endl;
+}
---
+++ multipleSources/head.h
@@ -0,0 +1,33 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+//TRY TO KEEP INCLUDE LIBRARIES IN HEADER UNLESS NEED IT DIRECTLY FOR SPECIFIC CPP FILES
+//Include Guard is the main reason.
+//Library Directories
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include<ctime>
+#include<cctype>
+#include<cstdlib>
+
+//Declare global variables and functions in header
+
+//Function Declarations
+void helloWorld();
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
